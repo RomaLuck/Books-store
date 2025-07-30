e3e016f836dff2cbab051790a9698add89c9444c
romaluck <romeo91222@gmail.com>
2025-07-30 20:40:27 +0200

    add intro images

---
+++ app/components/Intro.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="intro">
+    <div class="intro__hero">
+      <div class="intro__hero-title">
+        Buy and sell your
+        books
+        <span class="highlight">for the best
+        prices</span>
+      </div>
+      <div class="intro__hero-description">
+        Find and read more your'll love, and keep track of the books you want to read.
+        Be part of the word's largeest community of book lovers on Goodreads.
+      </div>
+      <div class="intro__hero-search input-wrapper">
+        <input type="text" class="intro__input input" placeholder="Search..."/>
+      </div>
+    </div>
+    <div class="intro__image">
+      <img class="intro__image-first" src="/images/intro/Background1.png" alt="intro-book-first" loading="lazy">
+      <img class="intro__image-second" src="/images/intro/Background2.png" alt="intro-book-second" loading="lazy">
+      <img class="intro__image-third" src="/images/intro/Background3.png" alt="intro-book-third" loading="lazy">
+      <img class="intro__image-fourth" src="/images/intro/Background4.png" alt="intro-book-fourth" loading="lazy">
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.intro {
+  min-height: 570px;
+  font-family: Segoe UI, sans-serif;
+  background-color: #E4E4E7;
+  color: #1E1B4B;
+  display: flex;
+  padding: 87px 68px;
+  column-gap: 200px;
+
+  &__hero {
+    display: flex;
+    flex-direction: column;
+    max-width: 560px;
+    row-gap: 16px;
+
+    &-search {
+      width: 300px;
+    }
+
+    &-title {
+      font-style: normal;
+      font-weight: 700;
+      font-size: 56px;
+      line-height: 74px;
+
+      .highlight {
+        color: #6366F1;
+      }
+    }
+  }
+
+  &__image {
+    position: relative;
+
+    img {
+      position: absolute;
+    }
+
+    &-first{
+      z-index: 5;
+    }
+    &-second{
+      z-index: 4;
+      left: 25px;
+    }
+
+    &-third{
+      z-index: 3;
+      left: 44px;
+    }
+
+    &-fourth{
+      z-index: 2;
+      left: 60px;
+    }
+  }
+
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,6 +4,7 @@
 
 <template>
   <Navigation/>
+  <Intro/>
   <slot/>
   <Footer/>
 </template>
